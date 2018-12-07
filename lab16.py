7a53b52d5ce96cd84eadc26e014d92b1396c96ba
evilmurries <cpiwarski00@gmail.com>
2018-12-06 18:08:06 -0800

    fixed urllib stuff

--- lab16.py
+++ lab16.py
@@ -1,6 +1,6 @@
 # Lab16 - Team 10
 # M. Mariscal, C. Piwarski, W. Robleh
-# Developed with Python2 and JES
+# Developed with Python3
 
 ################################################################################
 # 1. Choose a website that you frequent that has data (text) that could be 
@@ -16,52 +16,52 @@
 ################################################################################
 
 
-import urllib2
+import urllib.request
 import os
 
 # This function assembles a new HTML document with the supplied information
 def makePage(info):
-    path = os.path.pathabs(os.path.getcwd())
-    file = path + 'newPage.html'
-    print file
+    path = os.path.abspath(os.getcwd())
+    print(path)
+    file = path + '/newPage.html'
     header = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transition//EN" \
     "http://www.w3.org/TR/html4/loose.dtd">'
     title = '<html>\n<head><title> Title </title>\n</head>'
     bodyOpen = '<body>\n<h1>'
     bodyClose = '</body>\n</html>'
     try:
-        file = open(file, 'rw')
-        file.write(header)
-        file.write(title)
-        file.write(bodyOpen)
-        file.write('Testing!!!')
-        file.write(bodyClose)
-        file.close()
+        with open(file, 'w') as htmlPage:
+            htmlPage.write(header)
+            htmlPage.write(title)
+            htmlPage.write(bodyOpen)
+            htmlPage.write('Testing!!!')
+            htmlPage.write(bodyClose)
+            htmlPage.close()
     except IOError:
-        print 'Error Creating HTML File. Aborting...'
+        print('Error Creating HTML File. Aborting...')
 
 # Main function for the program
 def main():
 
     # declare variables
-    url = 'https://news.google.com/'
+    URL = 'https://www.python.org'
+    HEADERS = {'User-Agent': "Mozilla/4.0"}
 
     # open webpage
     try:
-        webPage = urllib2.urlopen(url)
-        html = webPage.read()
-        # There should be a decode statement. Have to figure it out for python2
-    except urllib2.HTTPError:
-        print 'HTTP Error Detected. Program Ending.'
-        raise SystemExit
-    except urllib2.URLError:
-        print 'URL Error Detected. Program Ending.'
+        request = urllib.request.Request(URL, headers=HEADERS)
+        web_page_request = urllib.request.urlopen(request)
+        web_page = web_page_request.read()
+        web_page = web_page.decode(encoding='utf-8')
+    except Exception as e:
+        print('Error has occurred, program ending. Error data is as follows:')
+        print(str(e))
         raise SystemExit
 
     # collect information
 
     # make new page
-    makePage(html)
+    makePage(web_page)
 
 if __name__ == '__main__':
     main()
